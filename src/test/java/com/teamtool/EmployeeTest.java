3580fa6f7bba59742d536ffe08dd2d023a2181ce
deeshavon <112518316+deeshavon@users.noreply.github.com>
2022-10-04 12:32:09 -0700

    TODOs for each method's testing

--- src/test/java/com/teamtool/EmployeeTest.java
+++ src/test/java/com/teamtool/EmployeeTest.java
@@ -3,74 +3,24 @@ package com.teamtool;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+
+import java.util.Collection;
+import java.util.Scanner;
 
 import java.io.FileReader;
 import java.io.IOException;
 
-public class EmployeeTest {
+class EmployeeTest {
 
   //should a Map be included for the test class? if yes, which one?
 
   //also all annotated @tests below are subject to group members' approval. This is simply a draft template to collaborate on.
 
 
-  @Test
-  public void query() throws IOException {
 
   }
 
-  @Test
-  void findViaId() {
-    //final Employee ?????? = this.xxxxx.findbyId();
-    //assertThat(employee.getId()).isEqualTo(#);
-    //assertThat(employee.getFirstName()).isEqualTo("Nick");
-    //assertThat(employee.getLastName()).isEqualTo("Bennett");
-
-  }
-  @Test
-  void findAll() {
-    //final EmployeeInfo employee-information = new EmployeeInfo();
-    // I think a Comparator or Comparable goes here but not sure which one. :-|
-    //assert????(employees).hasSize(#);
-  }
-
-  @Test
-  void deleteEmployeeByFirstName() {
-    FileReader.deleteByFirstName("deja");
-    assertTrue();
 
 
-    // final EmployeeInfo  employee-information = new EmployeeInfo();
-    // employee-information.setFirstName("?");
-    // Comparator or Comparable goes here?
-    // assert????(employees).hasSize(#);
-  }
-
-  @Test
-  void findViaLastName() {
-    //final EmployeeInfo employee-information = new EmployeeInfo();
-    // employee-information.setLastName("?");
-    // Comparator or Comparable goes here?
-    // asser
-  }
-
-
-
-  /* @Test
-  void testEmploymentStatus() {
-
-  }
-
-  @Test
-  void testEmployeeIdValue() {
-
-
-  }
-  @Test
-  void testEmployeeName() {
-
-  }
-  */
-
-
-}
---
+++ src/test/java/com/teamtool/ManagerTest.java
@@ -0,0 +1,38 @@
+package com.teamtool;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ManagerTest {
+
+    @Test
+    public void query() {
+        Manager m = new Manager();
+        // TODO: 10/4/22 create new instance of Manager
+        // TODO: 10/4/22 use instance to invoke query method
+        // TODO: 10/4/22 assertEquals result with expected
+    }
+
+    @Test
+    public void addEmployee() {
+        // TODO: 10/4/22 create new instance of Employee
+        // TODO: 10/4/22 use instance to invoke query method
+        // TODO: 10/4/22 assertEquals result with expected
+
+    }
+
+    @Test
+    public void changeEmployee() {
+        // TODO: 10/4/22 create new instance of Employee
+        // TODO: 10/4/22 use instance to invoke query method
+        // TODO: 10/4/22 assertEquals result with expected
+    }
+
+    @Test
+    public void deleteEmployee() {
+        // TODO: 10/4/22 create new instance of Employee
+        // TODO: 10/4/22 use instance to invoke query method
+        // TODO: 10/4/22 assertEquals result with expected
+    }
+}
